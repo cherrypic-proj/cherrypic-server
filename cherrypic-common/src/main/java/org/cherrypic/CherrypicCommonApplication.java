70affa8efaaca825b762f31d58c75ab3592a1c61
현태 최 <ht3064@naver.com>
2025-06-25 11:44:36 +0900

    feat: common 모듈 구현

---
+++ cherrypic-common/src/main/java/org/cherrypic/CherrypicCommonApplication.java
@@ -0,0 +1,13 @@
+package org.cherrypic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherrypicCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CherrypicCommonApplication.class, args);
+    }
+
+}
