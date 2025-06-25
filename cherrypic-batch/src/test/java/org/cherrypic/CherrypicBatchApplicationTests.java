00830faa0242928c6e85f3097e3d4abc7e06ad41
Hyuntaei Choi <145987233+ht3064@users.noreply.github.com>
2025-06-25 23:13:00 +0900

    chore: 프로젝트 초기 세팅 (#2)

---
+++ cherrypic-batch/src/test/java/org/cherrypic/CherrypicBatchApplicationTests.java
@@ -0,0 +1,11 @@
+package org.cherrypic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherrypicBatchApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
