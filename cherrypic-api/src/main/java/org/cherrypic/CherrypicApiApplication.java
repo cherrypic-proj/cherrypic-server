8e70227dc2b0eca4c7e5370f58420adf66b1e09d
현태 최 <ht3064@naver.com>
2025-06-25 11:25:22 +0900

    feat: api 모듈 구현

---
+++ cherrypic-api/src/main/java/org/cherrypic/CherrypicApiApplication.java
@@ -0,0 +1,13 @@
+package org.cherrypic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherrypicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CherrypicApiApplication.class, args);
+    }
+
+}
