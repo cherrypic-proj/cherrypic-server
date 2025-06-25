c990da7e1af54b84ecc5129396c9c1baf6803898
현태 최 <ht3064@naver.com>
2025-06-25 11:28:28 +0900

    feat: batch 모듈 구현

---
+++ cherrypic-batch/src/main/java/org/cherrypic/CherrypicBatchApplication.java
@@ -0,0 +1,13 @@
+package org.cherrypic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherrypicBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CherrypicBatchApplication.class, args);
+    }
+
+}
