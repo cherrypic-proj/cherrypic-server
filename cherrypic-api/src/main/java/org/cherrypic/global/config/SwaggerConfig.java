6d241f92dae8840829bd73295ace9c5e049d94c8
현태 최 <ht3064@naver.com>
2025-06-29 17:35:07 +0900

    feat: Swagger Authorization 헤더 추가

--- cherrypic-api/src/main/java/org/cherrypic/global/config/SwaggerConfig.java
+++ cherrypic-api/src/main/java/org/cherrypic/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package org.cherrypic.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +24,26 @@ public class SwaggerConfig {
                         new Info()
                                 .title("CherryPic Server API")
                                 .description("CherryPic Server API 명세서입니다.")
-                                .version("v0.0.1"));
+                                .version("v0.0.1"))
+                .components(authSetting())
+                .addSecurityItem(securityRequirement());
+    }
+
+    private Components authSetting() {
+        return new Components()
+                .addSecuritySchemes(
+                        "accessToken",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization"));
+    }
+
+    private SecurityRequirement securityRequirement() {
+        SecurityRequirement securityRequirement = new SecurityRequirement();
+        securityRequirement.addList("accessToken");
+        return securityRequirement;
     }
 }
