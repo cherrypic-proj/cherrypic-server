c66e93e3509381d43cce4ffc02bfbcafc9d95002
현태 최 <ht3064@naver.com>
2025-06-30 23:44:44 +0900

    refactor: GroupedOpenApi 설정 제거

--- cherrypic-api/src/main/java/org/cherrypic/global/config/swagger/SwaggerConfig.java
+++ cherrypic-api/src/main/java/org/cherrypic/global/config/swagger/SwaggerConfig.java
@@ -5,17 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder().group("v1").pathsToMatch("/**").build();
-    }
 
     @Bean
     public OpenAPI openAPI() {
