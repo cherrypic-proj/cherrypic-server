6632f45d88351f3209cd6a0ed1194a32e818881e
현태 최 <ht3064@naver.com>
2025-07-03 18:24:47 +0900

    feat: profile별 API 서버 URL 동적 적용

--- cherrypic-api/src/main/java/org/cherrypic/global/config/swagger/SwaggerConfig.java
+++ cherrypic-api/src/main/java/org/cherrypic/global/config/swagger/SwaggerConfig.java
@@ -5,11 +5,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.cherrypic.helper.SpringEnvironmentHelper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    private final SpringEnvironmentHelper springEnvironmentHelper;
 
     @Bean
     public OpenAPI openAPI() {
@@ -19,8 +26,21 @@ public class SwaggerConfig {
                                 .title("CherryPic Server API")
                                 .description("CherryPic Server API 명세서입니다.")
                                 .version("v0.0.1"))
+                .servers(initializeSwaggerServers())
                 .components(authSetting())
                 .addSecurityItem(securityRequirement());
+    }
+
+    private List<Server> initializeSwaggerServers() {
+        return List.of(new Server().url(resolveServerUrlByProfile()));
+    }
+
+    private String resolveServerUrlByProfile() {
+        return switch (springEnvironmentHelper.getCurrentProfile()) {
+            case "prod" -> "https://api.cherrypic.today";
+            case "dev" -> "https://dev-api.cherrypic.today";
+            default -> "http://localhost:8080";
+        };
     }
 
     private Components authSetting() {
