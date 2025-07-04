a714690ea3a1b1b9bfa3e261dc44517880c85357
Hyuntaei Choi <145987233+ht3064@users.noreply.github.com>
2025-07-04 14:35:32 +0900

    chore: Spring Security 설정 (#23)

---
+++ cherrypic-api/src/main/java/org/cherrypic/global/config/security/SecurityConfig.java
@@ -0,0 +1,92 @@
+package org.cherrypic.global.config.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.cherrypic.helper.SpringEnvironmentHelper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final SpringEnvironmentHelper springEnvironmentHelper;
+
+    private void defaultFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    }
+
+    @Bean
+    @Order(1)
+    @Profile({"dev", "local"})
+    public SecurityFilterChain swaggerFilterChain(HttpSecurity http) throws Exception {
+        defaultFilterChain(http);
+
+        http.securityMatcher("/swagger-ui/**", "/v3/api-docs/**").httpBasic(withDefaults());
+
+        http.authorizeHttpRequests(
+                springEnvironmentHelper.isProdProfile() || springEnvironmentHelper.isDevProfile()
+                        ? auth -> auth.anyRequest().authenticated()
+                        : auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        defaultFilterChain(http);
+
+        http.authorizeHttpRequests(
+                auth ->
+                        auth.requestMatchers("/cherrypic-actuator/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        if (springEnvironmentHelper.isProdProfile()) {
+            configuration.setAllowedOriginPatterns(List.of("https://cherrypic.today"));
+        }
+
+        if (springEnvironmentHelper.isDevProfile()) {
+            configuration.setAllowedOriginPatterns(
+                    List.of(
+                            "https://dev.cherrypic.today",
+                            "https://dev-api.cherrypic.today",
+                            "http://localhost:3000"));
+        }
+
+        configuration.setAllowedMethods(
+                List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
