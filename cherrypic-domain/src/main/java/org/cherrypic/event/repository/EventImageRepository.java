79eb5fcdefe8b491947ee0b27e238de7d7d13eeb
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-07-04 21:08:50 +0900

    refactor : event repository 수정

--- cherrypic-domain/src/main/java/org/cherrypic/event/repository/EventImageRepository.java
+++
@@ -1,5 +0,0 @@
-package org.cherrypic.event.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventImageRepository extends JpaRepository<Long, EventImageRepository> {}
