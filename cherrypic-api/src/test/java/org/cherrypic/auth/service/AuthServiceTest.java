07092080921d4e7b5bc4367a116393181dd474ec
현태 최 <ht3064@naver.com>
2025-07-15 02:10:24 +0900

    test: 토큰 재발급 서비스 테스트 추가

--- cherrypic-api/src/test/java/org/cherrypic/auth/service/AuthServiceTest.java
+++ cherrypic-api/src/test/java/org/cherrypic/auth/service/AuthServiceTest.java
@@ -1,24 +1,34 @@
 package org.cherrypic.auth.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import org.cherrypic.IntegrationTest;
+import org.cherrypic.domain.auth.dto.AccessTokenDto;
+import org.cherrypic.domain.auth.dto.RefreshTokenDto;
 import org.cherrypic.domain.auth.dto.request.IdTokenRequest;
 import org.cherrypic.domain.auth.dto.response.SocialLoginResponse;
+import org.cherrypic.domain.auth.dto.response.TokenReissueResponse;
 import org.cherrypic.domain.auth.enums.OauthProvider;
+import org.cherrypic.domain.auth.exception.AuthErrorCode;
+import org.cherrypic.domain.auth.exception.AuthException;
 import org.cherrypic.domain.auth.service.AuthService;
 import org.cherrypic.domain.auth.service.IdTokenVerifier;
 import org.cherrypic.domain.auth.service.JwtTokenService;
 import org.cherrypic.member.entity.Member;
+import org.cherrypic.member.entity.OauthInfo;
 import org.cherrypic.member.enums.MemberRole;
 import org.cherrypic.member.enums.MemberStatus;
 import org.cherrypic.member.repository.MemberRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -91,6 +101,52 @@ public class AuthServiceTest extends IntegrationTest {
                             Map.of("sub", sub, "iss", iss));
 
             return new DefaultOidcUser(List.of(), idToken);
+        }
+    }
+
+    @Nested
+    class 토큰_재발급할_때 {
+
+        @BeforeEach
+        void setUp() {
+            memberRepository.save(
+                    Member.createMember(
+                            OauthInfo.createOauthInfo("testOauthId", "testOauthProvider"),
+                            "testNickname",
+                            "testProfileImageUrl"));
+        }
+
+        @Test
+        void 유효한_리프레시_토큰이면_새로운_토큰을_반환한다() {
+            // given
+            RefreshTokenDto oldRefreshTokenDto =
+                    RefreshTokenDto.of(1L, "fake-old-register-token", 604800L);
+            RefreshTokenDto newRefreshTokenDto =
+                    RefreshTokenDto.of(1L, "fake-new-refresh-token", 604800L);
+            AccessTokenDto newAccessTokenDto =
+                    AccessTokenDto.of(1L, MemberRole.USER, "fake-new-access-token");
+
+            given(jwtTokenService.retrieveRefreshToken(anyString())).willReturn(oldRefreshTokenDto);
+            given(jwtTokenService.reissueRefreshToken(oldRefreshTokenDto))
+                    .willReturn(newRefreshTokenDto);
+            given(jwtTokenService.reissueAccessToken(any())).willReturn(newAccessTokenDto);
+
+            // when
+            TokenReissueResponse response = authService.reissueToken("testRefreshTokenValue");
+
+            // then
+            Assertions.assertAll(
+                    () -> assertThat(response.accessToken()).isEqualTo("fake-new-access-token"),
+                    () -> assertThat(response.refreshToken()).isEqualTo("fake-new-refresh-token"));
+        }
+
+        @Test
+        void 만료된_리프레시_토큰이면_예외가_발생한다() {
+            // given
+            assertThatThrownBy(() -> authService.reissueToken("testRefreshTokenValue"))
+                    .isInstanceOf(AuthException.class)
+                    .hasMessage(AuthErrorCode.EXPIRED_REFRESH_TOKEN.getMessage());
+            verify(jwtTokenService, times(1)).retrieveRefreshToken(anyString());
         }
     }
 }
