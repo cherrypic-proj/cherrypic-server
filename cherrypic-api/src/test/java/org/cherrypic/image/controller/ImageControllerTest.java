0129920ef7651724f1aa15ae6ca10ac01b4fd218
현태 최 <ht3064@naver.com>
2025-07-19 03:07:30 +0900

    test: 회원 프로필 이미지 Presigned URL 생성 컨트롤러 테스트 추가

---
+++ cherrypic-api/src/test/java/org/cherrypic/image/controller/ImageControllerTest.java
@@ -0,0 +1,83 @@
+package org.cherrypic.image.controller;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.cherrypic.domain.image.controller.ImageController;
+import org.cherrypic.domain.image.dto.request.MemberProfileImageUploadRequest;
+import org.cherrypic.domain.image.dto.response.PresignedUrlResponse;
+import org.cherrypic.domain.image.enums.ImageFileExtension;
+import org.cherrypic.domain.image.service.ImageService;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+@WebMvcTest(ImageController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class ImageControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+    @Autowired private ObjectMapper objectMapper;
+
+    @MockitoBean private ImageService imageService;
+
+    @Nested
+    class Presigned_URL_생성할_때 {
+
+        @Test
+        void 정상적인_회원_프로필_이미지_업로드_요청이면_Presigned_URL을_반환한다() throws Exception {
+            // given
+            MemberProfileImageUploadRequest request =
+                    new MemberProfileImageUploadRequest(ImageFileExtension.JPEG);
+
+            PresignedUrlResponse response = new PresignedUrlResponse("testPresignedUrl");
+
+            given(imageService.createMemberProfileImageUploadUrl(request)).willReturn(response);
+
+            // when & then
+            ResultActions perform =
+                    mockMvc.perform(
+                            post("/members/me/upload-url")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(request)));
+
+            perform.andExpect(status().isOk())
+                    .andExpect(jsonPath("$.success").value(true))
+                    .andExpect(jsonPath("$.status").value(200))
+                    .andExpect(jsonPath("$.data.presignedUrl").isNotEmpty());
+        }
+
+        @Test
+        void 이미지_파일_확장자가_null이면_예외가_발생한다() throws Exception {
+            // given
+            MemberProfileImageUploadRequest request = new MemberProfileImageUploadRequest(null);
+
+            PresignedUrlResponse response = new PresignedUrlResponse("testPresignedUrl");
+
+            given(imageService.createMemberProfileImageUploadUrl(request)).willReturn(response);
+
+            // when & then
+            ResultActions perform =
+                    mockMvc.perform(
+                            post("/members/me/upload-url")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(request)));
+
+            perform.andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+                    .andExpect(jsonPath("$.data.code").value("MethodArgumentNotValidException"))
+                    .andExpect(jsonPath("$.data.message").value("이미지 파일의 확장자는 비워둘 수 없습니다."));
+        }
+    }
+}
