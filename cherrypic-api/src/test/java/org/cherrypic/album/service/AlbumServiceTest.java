6ef93a8b81f4aca40b57a3ce2ec006bc7cc163ba
현태 최 <ht3064@naver.com>
2025-07-19 19:23:14 +0900

    test: 앨범 생성 서비스 테스트 추가

---
+++ cherrypic-api/src/test/java/org/cherrypic/album/service/AlbumServiceTest.java
@@ -0,0 +1,73 @@
+package org.cherrypic.album.service;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.cherrypic.IntegrationTest;
+import org.cherrypic.album.entity.Album;
+import org.cherrypic.album.enums.AlbumType;
+import org.cherrypic.album.repository.AlbumRepository;
+import org.cherrypic.domain.album.dto.request.AlbumCreateRequest;
+import org.cherrypic.domain.album.service.AlbumService;
+import org.cherrypic.member.entity.Member;
+import org.cherrypic.member.entity.OauthInfo;
+import org.cherrypic.member.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+class AlbumServiceTest extends IntegrationTest {
+
+    @Autowired private AlbumService albumService;
+    @Autowired private AlbumRepository albumRepository;
+    @Autowired private MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        Member member =
+                Member.createMember(
+                        OauthInfo.createOauthInfo("testOauthId", "testOauthProvider"),
+                        "testNickname",
+                        "testProfileImageUrl");
+        memberRepository.save(member);
+
+        UserDetails userDetails =
+                User.withUsername(member.getId().toString())
+                        .password("")
+                        .authorities(member.getRole().name())
+                        .build();
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(token);
+    }
+
+    @Nested
+    class 앨범을_생성할_때 {
+
+        @Test
+        void 유효한_요청이면_앨범을_생성한다() {
+
+            // given
+            AlbumCreateRequest request =
+                    new AlbumCreateRequest("testTitle", "testCoverUrl", AlbumType.SHARED);
+
+            // when
+            albumService.createAlbum(request);
+
+            // then
+            Album album = albumRepository.findById(1L).orElseThrow();
+
+            Assertions.assertAll(
+                    () -> assertThat(album.getId()).isEqualTo(1L),
+                    () -> assertThat(album.getTitle()).isEqualTo("testTitle"),
+                    () -> assertThat(album.getCoverUrl()).isEqualTo("testCoverUrl"),
+                    () -> assertThat(album.getType()).isEqualTo(AlbumType.SHARED));
+        }
+    }
+}
