18b6642215ce85e3348f1cb57ee046e0b2783ee0
현태 최 <ht3064@naver.com>
2025-07-20 00:17:14 +0900

    test: 앨범 생성 테스트에 HOST 참여자 검증 로직 추가

--- cherrypic-api/src/test/java/org/cherrypic/album/service/AlbumServiceTest.java
+++ cherrypic-api/src/test/java/org/cherrypic/album/service/AlbumServiceTest.java
@@ -11,6 +11,8 @@ import org.cherrypic.domain.album.service.AlbumService;
 import org.cherrypic.member.entity.Member;
 import org.cherrypic.member.entity.OauthInfo;
 import org.cherrypic.member.repository.MemberRepository;
+import org.cherrypic.participant.entity.Participant;
+import org.cherrypic.participant.enums.ParticipantRole;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -51,7 +53,7 @@ class AlbumServiceTest extends IntegrationTest {
     class 앨범을_생성할_때 {
 
         @Test
-        void 유효한_요청이면_앨범을_생성한다() {
+        void 유효한_요청이면_앨범과_HOST_참여자가_생성된다() {
 
             // given
             AlbumCreateRequest request =
@@ -62,12 +64,15 @@ class AlbumServiceTest extends IntegrationTest {
 
             // then
             Album album = albumRepository.findById(1L).orElseThrow();
+            Participant participant = album.getParticipants().get(0);
 
             Assertions.assertAll(
                     () -> assertThat(album.getId()).isEqualTo(1L),
                     () -> assertThat(album.getTitle()).isEqualTo("testTitle"),
                     () -> assertThat(album.getCoverUrl()).isEqualTo("testCoverUrl"),
-                    () -> assertThat(album.getType()).isEqualTo(AlbumType.SHARED));
+                    () -> assertThat(album.getType()).isEqualTo(AlbumType.SHARED),
+                    () -> assertThat(participant.getId()).isEqualTo(1L),
+                    () -> assertThat(participant.getRole()).isEqualTo(ParticipantRole.HOST));
         }
     }
 }
