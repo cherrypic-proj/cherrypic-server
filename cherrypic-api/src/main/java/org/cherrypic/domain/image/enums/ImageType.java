618913d38ba754edb612c412e936b7bd805a8125
현태 최 <ht3064@naver.com>
2025-07-20 16:23:09 +0900

    feat: ImageType enum에 description 필드 추가 및 type을 kebab-case로 변경

--- cherrypic-api/src/main/java/org/cherrypic/domain/image/enums/ImageType.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/image/enums/ImageType.java
@@ -6,11 +6,12 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ImageType {
-    MEMBER_PROFILE("member_profile"),
-    ALBUM_COVER("album_cover"),
-    ALBUM_IMAGE("album_image"),
-    EVENT_IMAGE("event_image"),
+    MEMBER_PROFILE("member-profile", "회원 프로필 사진"),
+    ALBUM_COVER("album-cover", "앨범 커버"),
+    ALBUM_IMAGE("album-image", "앨범 사진"),
+    EVENT_IMAGE("event-image", "이벤트 사진"),
     ;
 
     private final String type;
+    private final String description;
 }
