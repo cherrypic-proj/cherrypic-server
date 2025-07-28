5c12dbf02cf5d255b069327f8c8380ed64048dd6
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-07-29 00:12:25 +0900

    fix: 오타 수정

--- cherrypic-api/src/main/java/org/cherrypic/domain/event/exception/EventErrorCode.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/event/exception/EventErrorCode.java
@@ -7,7 +7,7 @@ import org.cherrypic.exception.BaseErrorCode;
 @Getter
 @AllArgsConstructor
 public enum EventErrorCode implements BaseErrorCode {
-    EVENT_NOT_FOUND(404, "존재하지 않는 이밴트입니다.");
+    EVENT_NOT_FOUND(404, "존재하지 않는 이벤트입니다.");
 
     private final int status;
     private final String message;
