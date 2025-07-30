31aba73982532ff6aeea216e3c59f450ec1fd824
현태 최 <ht3064@naver.com>
2025-07-30 12:53:15 +0900

    refactor: 불필요한 메서드 제거

--- cherrypic-api/src/main/java/org/cherrypic/domain/participant/repository/ParticipantRepository.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/participant/repository/ParticipantRepository.java
@@ -5,7 +5,5 @@ import org.cherrypic.participant.entity.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
-    boolean existsByMemberIdAndAlbumId(Long memberId, Long albumId);
-
     Optional<Participant> findByMemberIdAndAlbumId(Long memberId, Long albumId);
 }
