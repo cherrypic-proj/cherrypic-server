8e29957dda3cfcf468d758f4629a7fa7796f0bbd
현태 최 <ht3064@naver.com>
2025-07-30 18:33:52 +0900

    feat: 참여 앨범 목록 커서 기반 조회 쿼리 구현

--- cherrypic-api/src/main/java/org/cherrypic/domain/album/repository/AlbumRepositoryCustom.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/album/repository/AlbumRepositoryCustom.java
@@ -1,3 +1,10 @@
 package org.cherrypic.domain.album.repository;
 
-public interface AlbumRepositoryCustom {}
+import org.cherrypic.domain.album.dto.response.AlbumListResponse;
+import org.cherrypic.global.pagination.SortDirection;
+import org.springframework.data.domain.Slice;
+
+public interface AlbumRepositoryCustom {
+    Slice<AlbumListResponse> findAllByMemberId(
+            Long memberId, Long lastAlbumId, int size, SortDirection direction);
+}
---
+++ cherrypic-api/src/main/java/org/cherrypic/global/pagination/SortDirection.java
@@ -0,0 +1,6 @@
+package org.cherrypic.global.pagination;
+
+public enum SortDirection {
+    ASC,
+    DESC
+}
