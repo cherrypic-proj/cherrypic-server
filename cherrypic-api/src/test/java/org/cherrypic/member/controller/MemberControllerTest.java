4cd58828de1ad70e101346a6cb1f0d62045c72bd
현태 최 <ht3064@naver.com>
2025-08-02 16:23:32 +0900

    refactor: 회원 컨트롤러 테스트 리팩토링

--- cherrypic-api/src/test/java/org/cherrypic/member/controller/MemberControllerTest.java
+++ cherrypic-api/src/test/java/org/cherrypic/member/controller/MemberControllerTest.java
@@ -10,11 +10,11 @@ import org.cherrypic.domain.member.dto.response.MemberInfoResponse;
 import org.cherrypic.domain.member.service.MemberService;
 import org.cherrypic.member.enums.MemberRole;
 import org.cherrypic.member.enums.MemberStatus;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -28,35 +28,35 @@ class MemberControllerTest {
 
     @MockitoBean private MemberService memberService;
 
-    @Test
-    void 회원_정보를_조회한다() throws Exception {
-        // given
-        MemberInfoResponse response =
-                new MemberInfoResponse(
-                        1L,
-                        "https://kauth.kakao.com",
-                        "testNickname",
-                        "testProfileImageUrl",
-                        MemberStatus.NORMAL,
-                        MemberRole.USER);
+    @Nested
+    class 회원_정보_조회_요청_시 {
 
-        given(memberService.getMemberInfo()).willReturn(response);
+        @Test
+        void 유효한_요청이면_회원_정보를_조회한다() throws Exception {
+            // given
+            MemberInfoResponse response =
+                    new MemberInfoResponse(
+                            1L,
+                            "https://kauth.kakao.com",
+                            "testNickname",
+                            "testProfileImageUrl",
+                            MemberStatus.NORMAL,
+                            MemberRole.USER);
 
-        // when & then
-        ResultActions perform =
-                mockMvc.perform(
-                        get("/members/me")
-                                .param("oauthProvider", "KAKAO")
-                                .contentType(MediaType.APPLICATION_JSON));
+            given(memberService.getMemberInfo()).willReturn(response);
 
-        perform.andExpect(status().isOk())
-                .andExpect(jsonPath("$.success").value(true))
-                .andExpect(jsonPath("$.status").value(200))
-                .andExpect(jsonPath("$.data.memberId").value(1))
-                .andExpect(jsonPath("$.data.oauthProvider").value("https://kauth.kakao.com"))
-                .andExpect(jsonPath("$.data.nickname").value("testNickname"))
-                .andExpect(jsonPath("$.data.profileImageUrl").value("testProfileImageUrl"))
-                .andExpect(jsonPath("$.data.status").value("NORMAL"))
-                .andExpect(jsonPath("$.data.role").value("USER"));
+            // when & then
+            ResultActions perform = mockMvc.perform(get("/members/me"));
+
+            perform.andExpect(status().isOk())
+                    .andExpect(jsonPath("$.success").value(true))
+                    .andExpect(jsonPath("$.status").value(200))
+                    .andExpect(jsonPath("$.data.memberId").value(1))
+                    .andExpect(jsonPath("$.data.oauthProvider").value("https://kauth.kakao.com"))
+                    .andExpect(jsonPath("$.data.nickname").value("testNickname"))
+                    .andExpect(jsonPath("$.data.profileImageUrl").value("testProfileImageUrl"))
+                    .andExpect(jsonPath("$.data.status").value("NORMAL"))
+                    .andExpect(jsonPath("$.data.role").value("USER"));
+        }
     }
 }
