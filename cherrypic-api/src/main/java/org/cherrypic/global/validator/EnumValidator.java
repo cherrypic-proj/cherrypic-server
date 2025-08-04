93e2674b6d15e39fb6f1d1b0fc46f70361aff2f9
Hyuntaei Choi <145987233+ht3064@users.noreply.github.com>
2025-08-04 15:54:11 +0900

    feat: Enum 값 유효성 검증 일괄 적용 (#89)
    * refactor: EnumValidator 단순 null 체크로 변경하여 검증 로직 간소화
    
    * refactor: Request DTO의 Enum 필드에 @NotNull 대신 커스텀 Enum 검증 어노테이션 적용
    
    * refactor: 앨범 목록 조회 API에 페이지 크기 검증용 커스텀 어노테이션 적용
    
    * test: 앨범 목록 조회 API 테스트에 페이지 크기 및 정렬 값 검증 테스트 추가

--- cherrypic-api/src/main/java/org/cherrypic/global/validator/EnumValidator.java
+++ cherrypic-api/src/main/java/org/cherrypic/global/validator/EnumValidator.java
@@ -2,14 +2,11 @@ package org.cherrypic.global.validator;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import java.util.Arrays;
 import org.cherrypic.global.annotation.Enum;
 
 public class EnumValidator implements ConstraintValidator<Enum, java.lang.Enum> {
     @Override
     public boolean isValid(java.lang.Enum value, ConstraintValidatorContext context) {
-        if (value == null) return false;
-        Class<?> reflectionEnumClass = value.getDeclaringClass();
-        return Arrays.asList(reflectionEnumClass.getEnumConstants()).contains(value);
+        return value != null;
     }
 }
