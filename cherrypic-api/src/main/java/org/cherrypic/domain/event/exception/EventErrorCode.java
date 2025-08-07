46c37a0d09d10e78dbec086723ebf99da793dbc0
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-08-08 00:26:49 +0900

    fix: 에러 메시지 수정

--- cherrypic-api/src/main/java/org/cherrypic/domain/event/exception/EventErrorCode.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/event/exception/EventErrorCode.java
@@ -8,7 +8,7 @@ import org.cherrypic.exception.BaseErrorCode;
 @AllArgsConstructor
 public enum EventErrorCode implements BaseErrorCode {
     EVENT_NOT_FOUND(404, "존재하지 않는 이벤트입니다."),
-    EVENT_NOT_FOUND_IN_ALBUM(404, "앨범에 존재하지 않는 앨범입니다");
+    EVENT_NOT_FOUND_IN_ALBUM(404, "앨범에 해당 이벤트가 존재하지 않습니다.");
 
     private final int status;
     private final String message;
