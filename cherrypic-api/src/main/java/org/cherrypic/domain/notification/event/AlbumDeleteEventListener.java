ac38681a2630212455528be5f069cf0f4aaa71c8
현태 최 <ht3064@naver.com>
2025-08-08 18:55:32 +0900

    feat: 이벤트 리스너를 롤백 확정 후 별도 트랜잭션에서 실행하도록 구성

--- cherrypic-api/src/main/java/org/cherrypic/domain/notification/event/AlbumDeleteEventListener.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/notification/event/AlbumDeleteEventListener.java
@@ -3,8 +3,11 @@ package org.cherrypic.domain.notification.event;
 import lombok.RequiredArgsConstructor;
 import org.cherrypic.domain.album.event.AlbumDeleteEvent;
 import org.cherrypic.domain.notification.service.NotificationService;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -12,7 +15,8 @@ public class AlbumDeleteEventListener {
 
     private final NotificationService notificationService;
 
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void handleAlbumDeleteEvent(AlbumDeleteEvent event) {
         notificationService.sendAlbumDeleteNotification(
                 event.albumId(), event.senderId(), event.receiverIds());
