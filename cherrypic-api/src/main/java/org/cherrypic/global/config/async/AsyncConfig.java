31501c29f54b8570c2b4b844de1c2dbc7691fa4d
현태 최 <ht3064@naver.com>
2025-08-08 17:05:20 +0900

    chore: @Async 처리를 위한 기본 비동기 설정 추가

---
+++ cherrypic-api/src/main/java/org/cherrypic/global/config/async/AsyncConfig.java
@@ -0,0 +1,8 @@
+package org.cherrypic.global.config.async;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {}
