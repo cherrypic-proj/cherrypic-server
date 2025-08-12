9f2c4290aa7fbb94d84da0e75033102ad5795192
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-08-12 22:17:17 +0900

    refactor: test 순서 정렬

--- cherrypic-api/src/test/java/org/cherrypic/event/controller/EventControllerTest.java
+++ cherrypic-api/src/test/java/org/cherrypic/event/controller/EventControllerTest.java
@@ -694,25 +694,6 @@ public class EventControllerTest {
         }
 
         @Test
-        void 추가할_이미지를_입력하지_않은_경우_예외가_발생한다() throws Exception {
-            // given
-            EventImageAddRequest request = new EventImageAddRequest(List.of());
-
-            // when & then
-            ResultActions perform =
-                    mockMvc.perform(
-                            post("/events/1/add-images")
-                                    .contentType(MediaType.APPLICATION_JSON)
-                                    .content(objectMapper.writeValueAsString(request)));
-
-            perform.andExpect(status().isBadRequest())
-                    .andExpect(jsonPath("$.success").value(false))
-                    .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
-                    .andExpect(jsonPath("$.data.code").value("MethodArgumentNotValidException"))
-                    .andExpect(jsonPath("$.data.message").value("추가할 이미지 ID는 비워둘 수 없습니다."));
-        }
-
-        @Test
         void 앨범에_이미지를_추가하던_와중_다른_사람이_해당_이미지를_조작하면_예외가_발생한다() throws Exception {
             // given
             EventImageAddRequest request = new EventImageAddRequest(List.of(1L));
@@ -732,6 +713,27 @@ public class EventControllerTest {
                     .andExpect(jsonPath("$.status").value(HttpStatus.CONFLICT.value()))
                     .andExpect(jsonPath("$.data.code").value("CONFLICTING_IMAGES"))
                     .andExpect(jsonPath("$.data.message").value("다른 요청에서 조작된 이미지를 포함하고 있습니다."));
+        }
+
+        @ParameterizedTest
+        @NullSource
+        @EmptySource
+        void 추가할_이미지를_입력하지_않은_경우_예외가_발생한다(List<Long> input) throws Exception {
+            // given
+            EventImageAddRequest request = new EventImageAddRequest(input);
+
+            // when & then
+            ResultActions perform =
+                    mockMvc.perform(
+                            post("/events/1/add-images")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(request)));
+
+            perform.andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
+                    .andExpect(jsonPath("$.data.code").value("MethodArgumentNotValidException"))
+                    .andExpect(jsonPath("$.data.message").value("추가할 이미지 ID는 비워둘 수 없습니다."));
         }
     }
 }
