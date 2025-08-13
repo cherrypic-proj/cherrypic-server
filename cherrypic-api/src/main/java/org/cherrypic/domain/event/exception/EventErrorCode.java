9cde43b10753bdb65244c5be5cdd7c618827abd9
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-08-14 03:27:10 +0900

    refactor: 에러 메시지 오타 수정 및 Image 관련 에러 메시지 수정

--- cherrypic-api/src/main/java/org/cherrypic/domain/event/exception/EventErrorCode.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/event/exception/EventErrorCode.java
@@ -8,7 +8,7 @@ import org.cherrypic.exception.BaseErrorCode;
 @AllArgsConstructor
 public enum EventErrorCode implements BaseErrorCode {
     EVENT_NOT_FOUND(404, "존재하지 않는 이벤트입니다."),
-    EVENT_DELETED(409, "이벤트 관련 작업중 이벤트가 삭제되었습니다.");
+    EVENT_DELETED(409, "이벤트 관련 작업 중 이벤트가 삭제되었습니다.");
 
     private final int status;
     private final String message;
--- cherrypic-api/src/main/java/org/cherrypic/domain/image/exception/ImageErrorCode.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/image/exception/ImageErrorCode.java
@@ -9,8 +9,8 @@ import org.cherrypic.exception.BaseErrorCode;
 public enum ImageErrorCode implements BaseErrorCode {
     IMAGES_NOT_FOUND(404, "존재하지 않는 이미지를 포함하고 있습니다."),
     IMAGES_FROM_OTHER_ALBUM(400, "앨범 소속이 아닌 이미지를 포함하고 있습니다."),
-    IMAGE_DELETED(409, "이미지 관련 작업중 이미지가 삭제되었습니다."),
-    IMAGE_CONFLICT(409, "이미지 동시성 문제 발생");
+    IMAGE_DELETED(409, "이미지 관련 작업 중 이미지가 삭제되었습니다."),
+    IMAGE_CONFLICT(409, "예상치 못한 이미지 무결성 오류");
 
     private final int status;
     private final String message;
