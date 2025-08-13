489722d36938330c44dd1e30f9e181dc55a39308
현태 최 <ht3064@naver.com>
2025-08-13 23:03:06 +0900

    test: 참가자 강퇴 기능 단위, 통합 테스트 추가

--- cherrypic-api/src/test/java/org/cherrypic/participant/service/ParticipantServiceTest.java
+++ cherrypic-api/src/test/java/org/cherrypic/participant/service/ParticipantServiceTest.java
@@ -11,10 +11,12 @@ import org.cherrypic.album.enums.AlbumPlan;
 import org.cherrypic.domain.album.exception.AlbumErrorCode;
 import org.cherrypic.domain.album.repository.AlbumRepository;
 import org.cherrypic.domain.member.repository.MemberRepository;
+import org.cherrypic.domain.participant.exception.ParticipantErrorCode;
 import org.cherrypic.domain.participant.repository.ParticipantRepository;
 import org.cherrypic.domain.participant.service.ParticipantService;
 import org.cherrypic.exception.CustomException;
 import org.cherrypic.global.util.MemberUtil;
+import org.cherrypic.global.util.TransactionUtil;
 import org.cherrypic.member.entity.Member;
 import org.cherrypic.member.entity.OauthInfo;
 import org.cherrypic.participant.entity.Participant;
@@ -24,6 +26,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 class ParticipantServiceTest extends IntegrationTest {
+
+    @Autowired private TransactionUtil transactionUtil;
 
     @Autowired private ParticipantService participantService;
     @Autowired private MemberRepository memberRepository;
@@ -88,6 +92,108 @@ class ParticipantServiceTest extends IntegrationTest {
             assertThatThrownBy(() -> participantService.leaveAlbum(3L))
                     .isInstanceOf(CustomException.class)
                     .hasMessage(AlbumErrorCode.HOST_LEAVE_NOT_ALLOWED.getMessage());
+        }
+    }
+
+    @Nested
+    class 참가자를_강퇴할_때 {
+
+        @BeforeEach
+        void setUp() {
+            Member member1 =
+                    Member.createMember(
+                            OauthInfo.createOauthInfo("testOauthId1", "testOauthProvider1"),
+                            "testNickname1",
+                            "testProfileImageUrl1");
+            Member member2 =
+                    Member.createMember(
+                            OauthInfo.createOauthInfo("testOauthId2", "testOauthProvider2"),
+                            "testNickname2",
+                            "testProfileImageUrl2");
+            memberRepository.saveAll(List.of(member1, member2));
+            given(memberUtil.getCurrentMember()).willReturn(member1);
+
+            Album album1 = Album.createAlbum("testAlbum1", "testURL1", AlbumPlan.BASIC, false);
+            Album album2 = Album.createAlbum("testAlbum2", "testURL2", AlbumPlan.BASIC, false);
+            Album album3 = Album.createAlbum("testAlbum3", "testURL3", AlbumPlan.BASIC, false);
+            albumRepository.saveAll(List.of(album1, album2, album3));
+
+            Participant participant1 =
+                    Participant.createParticipant(member1, album1, ParticipantRole.HOST);
+            Participant participant2 =
+                    Participant.createParticipant(member2, album1, ParticipantRole.STANDARD);
+            Participant participant3 =
+                    Participant.createParticipant(member2, album2, ParticipantRole.HOST);
+            Participant participant4 =
+                    Participant.createParticipant(member1, album2, ParticipantRole.STANDARD);
+            participantRepository.saveAll(
+                    List.of(participant1, participant2, participant3, participant4));
+        }
+
+        @Test
+        void 유효한_요청이면_앨범_방장이_참가자를_강퇴하여_앨범에서_제거한다() {
+            // when
+            participantService.kickParticipant(1L, 2L);
+
+            // then
+            Album album =
+                    transactionUtil.getResult(
+                            () -> {
+                                Album loadedAlbum = albumRepository.findById(1L).get();
+                                loadedAlbum.getParticipants().size();
+                                return loadedAlbum;
+                            });
+            List<Participant> participants = album.getParticipants();
+
+            assertThat(participants).extracting(Participant::getId).doesNotContain(2L);
+        }
+
+        @Test
+        void 앨범이_존재하지_않는_경우_예외가_발생한다() {
+            // when & then
+            assertThatThrownBy(() -> participantService.kickParticipant(999L, 2L))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(AlbumErrorCode.ALBUM_NOT_FOUND.getMessage());
+        }
+
+        @Test
+        void 강퇴_요청자가_앨범_참가자가_아닌_경우_예외가_발생한다() {
+            // when & then
+            assertThatThrownBy(() -> participantService.kickParticipant(3L, 2L))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(AlbumErrorCode.NOT_ALBUM_PARTICIPANT.getMessage());
+        }
+
+        @Test
+        void 강퇴_요청자가_앨범_방장이_아닌_경우_예외가_발생한다() {
+            // when & then
+            assertThatThrownBy(() -> participantService.kickParticipant(2L, 3L))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(AlbumErrorCode.NOT_ALBUM_HOST.getMessage());
+        }
+
+        @Test
+        void 강퇴_요청자와_강퇴_대상이_동일한_경우_예외가_발생한다() {
+            // when & then
+            assertThatThrownBy(() -> participantService.kickParticipant(1L, 1L))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(AlbumErrorCode.HOST_SELF_KICK_NOT_ALLOWED.getMessage());
+        }
+
+        @Test
+        void 강퇴_대상_참가자가_존재하지_않는_경우_예외가_발생한다() {
+            // when & then
+            assertThatThrownBy(() -> participantService.kickParticipant(1L, 999L))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(ParticipantErrorCode.PARTICIPANT_NOT_FOUND.getMessage());
+        }
+
+        @Test
+        void 강퇴_대상이_앨범_참가자가_아닌_경우_예외가_발생한다() {
+            // when & then
+            assertThatThrownBy(() -> participantService.kickParticipant(1L, 3L))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(AlbumErrorCode.PARTICIPANT_NOT_IN_ALBUM.getMessage());
         }
     }
 }
