776c6869ea7c43bab1d40e059b263469793eba01
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-08-24 13:23:49 +0900

    chore: 에러 메시지 복수형으로 수정

--- cherrypic-api/src/main/java/org/cherrypic/domain/image/dto/request/AlbumImageDeleteRequest.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/image/dto/request/AlbumImageDeleteRequest.java
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 
 public record AlbumImageDeleteRequest(
-        @NotEmpty(message = "삭제하고자 하는 이미지 ID는 비워둘 수 없습니다.")
+        @NotEmpty(message = "삭제하고자 하는 이미지 ID들은 비워둘 수 없습니다.")
                 @Schema(description = "삭제하고자 하는 이미지들의 ID", example = "[1,2,3,4]")
                 List<Long> imageIds) {}
