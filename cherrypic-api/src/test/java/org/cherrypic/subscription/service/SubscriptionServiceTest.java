8054882b316f6ceb2fcf5b2592dceae1ddcb86f1
현태 최 <ht3064@naver.com>
2025-08-27 01:43:02 +0900

    test: 구독 갱신 통합 테스트 추가

--- cherrypic-api/src/test/java/org/cherrypic/subscription/service/SubscriptionServiceTest.java
+++ cherrypic-api/src/test/java/org/cherrypic/subscription/service/SubscriptionServiceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.*;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import org.cherrypic.IntegrationTest;
 import org.cherrypic.album.entity.Album;
@@ -13,28 +14,40 @@ import org.cherrypic.domain.album.exception.AlbumErrorCode;
 import org.cherrypic.domain.album.repository.AlbumRepository;
 import org.cherrypic.domain.member.repository.MemberRepository;
 import org.cherrypic.domain.participant.repository.ParticipantRepository;
+import org.cherrypic.domain.payment.exception.PaymentErrorCode;
+import org.cherrypic.domain.payment.repository.PaymentRepository;
+import org.cherrypic.domain.subscription.dto.request.SubscriptionRenewRequest;
 import org.cherrypic.domain.subscription.exception.SubscriptionErrorCode;
 import org.cherrypic.domain.subscription.repository.SubscriptionRepository;
 import org.cherrypic.domain.subscription.service.SubscriptionService;
 import org.cherrypic.exception.CustomException;
 import org.cherrypic.global.util.MemberUtil;
+import org.cherrypic.global.util.TransactionUtil;
 import org.cherrypic.member.entity.Member;
 import org.cherrypic.member.entity.OauthInfo;
 import org.cherrypic.participant.entity.Participant;
 import org.cherrypic.participant.enums.ParticipantRole;
+import org.cherrypic.payment.entity.Payment;
+import org.cherrypic.payment.enums.PaymentPurpose;
+import org.cherrypic.payment.enums.PaymentStatus;
+import org.cherrypic.payment.exception.PaymentDomainErrorCode;
 import org.cherrypic.subscription.entity.Subscription;
 import org.cherrypic.subscription.enums.SubscriptionStatus;
+import org.cherrypic.subscription.exception.SubscriptionDomainErrorCode;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 class SubscriptionServiceTest extends IntegrationTest {
 
+    @Autowired private TransactionUtil transactionUtil;
+
     @Autowired private SubscriptionService subscriptionService;
     @Autowired private SubscriptionRepository subscriptionRepository;
     @Autowired private AlbumRepository albumRepository;
     @Autowired private MemberRepository memberRepository;
     @Autowired private ParticipantRepository participantRepository;
+    @Autowired private PaymentRepository paymentRepository;
 
     @MockitoBean private MemberUtil memberUtil;
 
@@ -144,7 +157,7 @@ class SubscriptionServiceTest extends IntegrationTest {
             // when & then
             assertThatThrownBy(() -> subscriptionService.cancelSubscription(2L))
                     .isInstanceOf(CustomException.class)
-                    .hasMessage(SubscriptionErrorCode.SUBSCRIPTION_ALREADY_CANCELED.getMessage());
+                    .hasMessage(SubscriptionDomainErrorCode.ALREADY_CANCELED.getMessage());
         }
 
         @Test
@@ -152,7 +165,289 @@ class SubscriptionServiceTest extends IntegrationTest {
             // when & then
             assertThatThrownBy(() -> subscriptionService.cancelSubscription(3L))
                     .isInstanceOf(CustomException.class)
-                    .hasMessage(SubscriptionErrorCode.SUBSCRIPTION_ALREADY_ENDED.getMessage());
+                    .hasMessage(SubscriptionDomainErrorCode.ALREADY_ENDED.getMessage());
+        }
+    }
+
+    @Nested
+    class 구독을_갱신할_때 {
+
+        @BeforeEach
+        void setUp() {
+            Member member1 =
+                    Member.createMember(
+                            OauthInfo.createOauthInfo("testOauthId1", "testOauthProvider1"),
+                            "testNickname1",
+                            "testProfileImageUrl1");
+            Member member2 =
+                    Member.createMember(
+                            OauthInfo.createOauthInfo("testOauthId2", "testOauthProvider2"),
+                            "testNickname2",
+                            "testProfileImageUrl2");
+            memberRepository.saveAll(List.of(member1, member2));
+            given(memberUtil.getCurrentMember()).willReturn(member1);
+
+            Album album1 = Album.createAlbum("testAlbum1", "testURL1", AlbumPlan.PRO, false);
+            Album album2 = Album.createAlbum("testAlbum2", "testURL2", AlbumPlan.PRO, false);
+            Album album3 = Album.createAlbum("testAlbum3", "testURL3", AlbumPlan.PRO, false);
+            Album album4 = Album.createAlbum("testAlbum4", "testURL4", AlbumPlan.BASIC, false);
+            Album album5 = Album.createAlbum("testAlbum5", "testURL5", AlbumPlan.PREMIUM, false);
+            Album album6 = Album.createAlbum("testAlbum6", "testURL6", AlbumPlan.PREMIUM, false);
+            Album album7 = Album.createAlbum("testAlbum7", "testURL7", AlbumPlan.PREMIUM, false);
+            albumRepository.saveAll(
+                    List.of(album1, album2, album3, album4, album5, album6, album7));
+
+            Participant participant1 =
+                    Participant.createParticipant(member1, album1, ParticipantRole.HOST);
+            Participant participant2 =
+                    Participant.createParticipant(member1, album3, ParticipantRole.STANDARD);
+            Participant participant3 =
+                    Participant.createParticipant(member1, album4, ParticipantRole.HOST);
+            Participant participant4 =
+                    Participant.createParticipant(member1, album5, ParticipantRole.HOST);
+            Participant participant5 =
+                    Participant.createParticipant(member1, album6, ParticipantRole.HOST);
+            Participant participant6 =
+                    Participant.createParticipant(member1, album7, ParticipantRole.HOST);
+            participantRepository.saveAll(
+                    List.of(
+                            participant1,
+                            participant2,
+                            participant3,
+                            participant4,
+                            participant5,
+                            participant6));
+
+            // 15일 전에 시작되어 현재는 해지된 구독
+            Subscription subscription1 =
+                    Subscription.createSubscription(
+                            member1, album1, LocalDateTime.now().minusDays(15));
+            subscription1.cancel();
+
+            // 구독 중
+            Subscription subscription3 =
+                    Subscription.createSubscription(member1, album6, LocalDateTime.now());
+
+            // 종료된 구독
+            Subscription subscription2 =
+                    Subscription.createSubscription(
+                            member1, album7, LocalDateTime.of(2025, 7, 1, 0, 0));
+
+            subscriptionRepository.saveAll(List.of(subscription1, subscription2, subscription3));
+
+            // 완료된 결제
+            Payment payment1 =
+                    Payment.createPayment(member1, "testMerchantUid", 5900, PaymentPurpose.RENEWAL);
+            payment1.updatePayment(
+                    "testImpUid", "testPgProvider", PaymentStatus.PAID, LocalDateTime.now());
+
+            // 완료된 다른 회원의 결제
+            Payment payment2 =
+                    Payment.createPayment(member2, "testMerchantUid", 5900, PaymentPurpose.RENEWAL);
+            payment2.updatePayment(
+                    "testImpUid", "testPgProvider", PaymentStatus.PAID, LocalDateTime.now());
+
+            // 완료되지 않은 결제
+            Payment payment3 =
+                    Payment.createPayment(member1, "testMerchantUid", 5900, PaymentPurpose.RENEWAL);
+
+            // 유료 앨범 생성에 쓰인 완료된 결제
+            Payment payment4 =
+                    Payment.createPayment(
+                            member1, "testMerchantUid", 5900, PaymentPurpose.CREATION);
+            payment4.updatePayment(
+                    "testImpUid", "testPgProvider", PaymentStatus.PAID, LocalDateTime.now());
+            payment4.updatePayment(PaymentPurpose.CREATION, album1);
+
+            // 구독 업그레이드 목적으로 쓰인 결제
+            Payment payment5 =
+                    Payment.createPayment(
+                            member1, "testMerchantUid", 12900, PaymentPurpose.UPGRADE);
+            payment5.updatePayment(
+                    "testImpUid", "testPgProvider", PaymentStatus.PAID, LocalDateTime.now());
+
+            paymentRepository.saveAll(List.of(payment1, payment2, payment3, payment4, payment5));
+        }
+
+        @Test
+        void 유효한_요청이면_구독을_갱신하고_결제에_앨범을_정상_연결한다() {
+            // given
+            Subscription canceledSubscription = subscriptionRepository.findById(1L).get();
+            LocalDateTime previousEndAt =
+                    canceledSubscription.getEndAt().truncatedTo(ChronoUnit.MINUTES);
+
+            SubscriptionRenewRequest request = new SubscriptionRenewRequest(1L);
+
+            // when
+            subscriptionService.renewSubscription(1L, request);
+
+            // then
+            Album album =
+                    transactionUtil.getResult(
+                            () -> {
+                                Album loadedAlbum = albumRepository.findById(1L).get();
+                                loadedAlbum.getPayments().get(0);
+                                loadedAlbum.getSubscription();
+                                return loadedAlbum;
+                            });
+            Payment payment = album.getPayments().get(0);
+            Subscription subscription = album.getSubscription();
+
+            Assertions.assertAll(
+                    () ->
+                            assertThat(payment)
+                                    .extracting("album.id", "status")
+                                    .containsExactly(1L, PaymentStatus.PAID),
+                    () ->
+                            assertThat(subscription)
+                                    .extracting("id", "member.id", "album.id", "status")
+                                    .containsExactly(1L, 1L, 1L, SubscriptionStatus.ACTIVE),
+                    () ->
+                            assertThat(subscription.getStartAt().truncatedTo(ChronoUnit.MINUTES))
+                                    .isEqualTo(previousEndAt.plusDays(1)),
+                    () ->
+                            assertThat(subscription.getEndAt().truncatedTo(ChronoUnit.MINUTES))
+                                    .isEqualTo(previousEndAt.plusMonths(1).plusDays(1)),
+                    () ->
+                            assertThat(
+                                            subscription
+                                                    .getNextBillingAt()
+                                                    .truncatedTo(ChronoUnit.MINUTES))
+                                    .isEqualTo(previousEndAt.plusMonths(1).plusDays(2)));
+        }
+
+        @Test
+        void 앨범이_존재하지_않는_경우_예외가_발생한다() {
+            // given
+            SubscriptionRenewRequest request = new SubscriptionRenewRequest(1L);
+
+            // when & then
+            assertThatThrownBy(() -> subscriptionService.renewSubscription(999L, request))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(AlbumErrorCode.ALBUM_NOT_FOUND.getMessage());
+        }
+
+        @Test
+        void 앨범_참가자가_아닌_경우_예외가_발생한다() {
+            // given
+            SubscriptionRenewRequest request = new SubscriptionRenewRequest(1L);
+
+            // when & then
+            assertThatThrownBy(() -> subscriptionService.renewSubscription(2L, request))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(AlbumErrorCode.NOT_ALBUM_PARTICIPANT.getMessage());
+        }
+
+        @Test
+        void 앨범_방장이_아닌_경우_예외가_발생한다() {
+            // given
+            SubscriptionRenewRequest request = new SubscriptionRenewRequest(1L);
+
+            // when & then
+            assertThatThrownBy(() -> subscriptionService.renewSubscription(3L, request))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(AlbumErrorCode.NOT_ALBUM_HOST.getMessage());
+        }
+
+        @Test
+        void BASIC_플랜인_경우_예외가_발생한다() {
+            // given
+            SubscriptionRenewRequest request = new SubscriptionRenewRequest(1L);
+
+            // when & then
+            assertThatThrownBy(() -> subscriptionService.renewSubscription(4L, request))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(
+                            SubscriptionErrorCode.SUBSCRIPTION_NOT_SUPPORTED_FOR_BASIC_PLAN
+                                    .getMessage());
+        }
+
+        @Test
+        void 결제가_존재하지_않는_경우_예외가_발생한다() {
+            // given
+            SubscriptionRenewRequest request = new SubscriptionRenewRequest(999L);
+
+            // when & then
+            assertThatThrownBy(() -> subscriptionService.renewSubscription(1L, request))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(PaymentErrorCode.PAYMENT_NOT_FOUND.getMessage());
+        }
+
+        @Test
+        void 결제한_회원과_구독을_갱신하려는_회원이_일치하지_않으면_예외가_발생한다() {
+            // given
+            SubscriptionRenewRequest request = new SubscriptionRenewRequest(2L);
+
+            // when & then
+            assertThatThrownBy(() -> subscriptionService.renewSubscription(1L, request))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(PaymentErrorCode.PAYMENT_MEMBER_MISMATCH.getMessage());
+        }
+
+        @Test
+        void 결제상태가_PAID가_아니면_예외가_발생한다() {
+            // given
+            SubscriptionRenewRequest request = new SubscriptionRenewRequest(3L);
+
+            // when & then
+            assertThatThrownBy(() -> subscriptionService.renewSubscription(1L, request))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(PaymentDomainErrorCode.NOT_PAID.getMessage());
+        }
+
+        @Test
+        void 결제가_이미_사용된_경우_예외가_발생한다() {
+            // given
+            SubscriptionRenewRequest request = new SubscriptionRenewRequest(4L);
+
+            // when & then
+            assertThatThrownBy(() -> subscriptionService.renewSubscription(1L, request))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(PaymentDomainErrorCode.ALREADY_USED_PAYMENT.getMessage());
+        }
+
+        @Test
+        void 결제의_목적이_구독_갱신과_일치하지_않으면_예외가_발생한다() {
+            // given
+            SubscriptionRenewRequest request = new SubscriptionRenewRequest(5L);
+
+            // when & then
+            assertThatThrownBy(() -> subscriptionService.renewSubscription(1L, request))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(PaymentDomainErrorCode.PAYMENT_PURPOSE_MISMATCH.getMessage());
+        }
+
+        @Test
+        void 구독이_존재하지_않는_경우_예외가_발생한다() {
+            // given
+            SubscriptionRenewRequest request = new SubscriptionRenewRequest(1L);
+
+            // when & then
+            assertThatThrownBy(() -> subscriptionService.renewSubscription(5L, request))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(SubscriptionErrorCode.SUBSCRIPTION_NOT_FOUND.getMessage());
+        }
+
+        @Test
+        void 이미_구독_중인_상태면_예외가_발생한다() {
+            // given
+            SubscriptionRenewRequest request = new SubscriptionRenewRequest(1L);
+
+            // when & then
+            assertThatThrownBy(() -> subscriptionService.renewSubscription(6L, request))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(SubscriptionDomainErrorCode.ALREADY_ACTIVE.getMessage());
+        }
+
+        @Test
+        void 종료된_구독이면_예외가_발생한다() {
+            // given
+            SubscriptionRenewRequest request = new SubscriptionRenewRequest(1L);
+
+            // when & then
+            assertThatThrownBy(() -> subscriptionService.renewSubscription(7L, request))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(SubscriptionDomainErrorCode.ALREADY_ENDED.getMessage());
         }
     }
 }
