4e5accf2b66a3604067a6af27be6482e8d7413da
현태 최 <ht3064@naver.com>
2025-08-28 15:12:37 +0900

    refactor: 구독 API에 앨범 유형 변경사항 반영

--- cherrypic-api/src/main/java/org/cherrypic/domain/subscription/exception/SubscriptionErrorCode.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/subscription/exception/SubscriptionErrorCode.java
@@ -8,7 +8,7 @@ import org.cherrypic.exception.BaseErrorCode;
 @AllArgsConstructor
 public enum SubscriptionErrorCode implements BaseErrorCode {
     SUBSCRIPTION_NOT_FOUND(404, "구독 정보가 존재하지 않습니다."),
-    SUBSCRIPTION_NOT_SUPPORTED_FOR_BASIC_PLAN(400, "BASIC 플랜은 구독 기능을 지원하지 않습니다."),
+    SUBSCRIPTION_NOT_SUPPORTED_FOR_BASIC_TYPE(400, "BASIC 유형은 구독 기능을 지원하지 않습니다."),
     ;
 
     private final int status;
