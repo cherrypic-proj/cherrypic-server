6af511d45a40bcb00ce6d572adbccabf8bf23523
현태 최 <ht3064@naver.com>
2025-09-02 20:33:10 +0900

    feat: 구독 정보 조회 API 구현

--- cherrypic-api/src/main/java/org/cherrypic/domain/subscription/service/SubscriptionService.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/subscription/service/SubscriptionService.java
@@ -1,10 +1,13 @@
 package org.cherrypic.domain.subscription.service;
 
 import org.cherrypic.domain.subscription.dto.request.SubscriptionRenewRequest;
+import org.cherrypic.domain.subscription.dto.response.SubscriptionInfoResponse;
 import org.cherrypic.domain.subscription.dto.response.SubscriptionRenewResponse;
 
 public interface SubscriptionService {
     void cancelSubscription(Long albumId);
 
     SubscriptionRenewResponse renewSubscription(Long albumId, SubscriptionRenewRequest request);
+
+    SubscriptionInfoResponse getSubscriptionInfo(Long albumId);
 }
