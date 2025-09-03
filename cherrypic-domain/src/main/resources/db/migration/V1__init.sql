a55576f0ef400d880a4d5aa92ad6948b10d4954e
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-09-03 16:14:05 +0900

    fix: Flyway 스크립트 수정

--- cherrypic-domain/src/main/resources/db/migration/V1__init.sql
+++ cherrypic-domain/src/main/resources/db/migration/V1__init.sql
@@ -134,8 +134,8 @@ CREATE TABLE temp_album (
                             type VARCHAR(255) CHECK (type IN ('DEFAULT')),
                             expired_at DATETIME NOT NULL,
                             url VARCHAR(255) NOT NULL,
-                            created_at DATETIME NOT NULL,
-                            updated_at DATETIME NOT NULL,
+                            created_at DATETIME(6) NOT NULL,
+                            updated_at DATETIME(6) NOT NULL,
 
                             CONSTRAINT fk_temp_album_member FOREIGN KEY (member_id) REFERENCES member(id)
 );
@@ -144,7 +144,7 @@ CREATE TABLE temp_album_image (
                                   id BIGINT AUTO_INCREMENT PRIMARY KEY,
                                   temp_album_id BIGINT NOT NULL,
                                   url VARCHAR(255) NOT NULL,
-                                  created_at DATETIME NOT NULL,
-                                  updated_at DATETIME NOT NULL,
-                                  CONSTRAINT fk_temp_album FOREIGN KEY (temp_album_id) REFERENCES temp_album(id) ON DELETE CASCADE
+                                  created_at DATETIME(6) NOT NULL,
+                                  updated_at DATETIME(6) NOT NULL,
+                                  CONSTRAINT fk_temp_album FOREIGN KEY (temp_album_id) REFERENCES temp_album(id)
 );
