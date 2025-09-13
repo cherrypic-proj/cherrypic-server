8f2a8bd8039a33e419c85a4a2c0ca197f0734b58
현태 최 <ht3064@naver.com>
2025-09-13 17:30:04 +0900

    refactor: 멤버별 권한 부여 상태 변경 API Swagger 설명 및 테스트 워딩 정리

--- cherrypic-api/src/main/java/org/cherrypic/domain/album/dto/response/PermissionToggleResponse.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/album/dto/response/PermissionToggleResponse.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import org.cherrypic.album.entity.Album;
 
 public record PermissionToggleResponse(
-        @Schema(description = "권한 부여 토글 상태", example = "true") Boolean permissionControl) {
+        @Schema(description = "권한 부여 허용 여부", example = "true") Boolean permissionControl) {
     public static PermissionToggleResponse from(Album album) {
         return new PermissionToggleResponse(album.getPermissionControl());
     }
