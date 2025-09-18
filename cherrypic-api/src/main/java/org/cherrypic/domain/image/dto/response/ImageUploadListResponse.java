9fc84478eca5498050019279420f9afba9e8cb1b
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-09-19 02:31:11 +0900

    refactor: imageUploadListResponse 메시지 정정

--- cherrypic-api/src/main/java/org/cherrypic/domain/image/dto/response/ImageUploadListResponse.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/image/dto/response/ImageUploadListResponse.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 public record ImageUploadListResponse(
-        @Schema(description = "로컬 사진 삭제 여부") Boolean localImageDeletion,
+        @Schema(description = "로컬 사진 삭제 허용 여부") Boolean localImageDeletion,
         @Schema(description = "업로드된 이미지들의 정보 리스트") List<Payload> payloads) {
     public static ImageUploadListResponse of(List<Payload> payloads, Boolean localImageDeletion) {
         return new ImageUploadListResponse(localImageDeletion, payloads);
