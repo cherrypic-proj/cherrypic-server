eb30584155170cd1e770e4f62a11345219aaa7a4
현태 최 <ht3064@naver.com>
2025-09-18 23:44:04 +0900

    refactor: 이벤트 조회 시 불필요한 left join 제거

--- cherrypic-api/src/main/java/org/cherrypic/domain/event/repository/EventRepository.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/event/repository/EventRepository.java
@@ -3,8 +3,10 @@ package org.cherrypic.domain.event.repository;
 import java.util.List;
 import org.cherrypic.event.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EventRepository extends JpaRepository<Event, Long>, EventRepositoryCustom {
-
-    List<Event> findAllByAlbumId(Long albumId);
+    @Query("select e from Event e where e.album.id = :albumId")
+    List<Event> findAllByAlbumId(@Param("albumId") Long albumId);
 }
