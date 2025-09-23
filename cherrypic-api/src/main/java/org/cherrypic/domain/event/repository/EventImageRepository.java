7f22150872cc93a764f9acaaea56bd644e1f4066
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-09-23 19:26:38 +0900

    fix: image 삭제시 EventImage 삭제 로직 추가

--- cherrypic-api/src/main/java/org/cherrypic/domain/event/repository/EventImageRepository.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/event/repository/EventImageRepository.java
@@ -3,6 +3,7 @@ package org.cherrypic.domain.event.repository;
 import java.util.List;
 import org.cherrypic.event.entity.Event;
 import org.cherrypic.event.entity.EventImage;
+import org.cherrypic.image.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +16,8 @@ public interface EventImageRepository extends JpaRepository<EventImage, Long> {
     @Modifying(clearAutomatically = true)
     @Query("delete from EventImage ei where ei.event = :event")
     void deleteAllByEvent(Event event);
+
+    @Modifying
+    @Query("delete from EventImage ei where ei.image in :images")
+    void deleteAllByImages(List<Image> images);
 }
