5d97701ac9117be33ab87fe3f975cc35b9ae1505
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-10-01 03:45:41 +0900

    test: 테스트 작성 완료

--- cherrypic-api/src/test/java/org/cherrypic/tempalbum/service/TempAlbumServiceTest.java
+++ cherrypic-api/src/test/java/org/cherrypic/tempalbum/service/TempAlbumServiceTest.java
@@ -6,9 +6,11 @@ import static org.mockito.BDDMockito.given;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import org.cherrypic.IntegrationTest;
 import org.cherrypic.domain.member.repository.MemberRepository;
 import org.cherrypic.domain.tempalbum.dto.request.TempAlbumCreateRequest;
+import org.cherrypic.domain.tempalbum.dto.response.TempAlbumListResponse;
 import org.cherrypic.domain.tempalbum.exception.TempAlbumErrorCode;
 import org.cherrypic.domain.tempalbum.repository.TempAlbumRepository;
 import org.cherrypic.domain.tempalbum.service.TempAlbumService;
@@ -18,6 +20,7 @@ import org.cherrypic.member.entity.Member;
 import org.cherrypic.member.entity.OauthInfo;
 import org.cherrypic.tempalbum.entity.TempAlbum;
 import org.cherrypic.tempalbum.enums.TempAlbumType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -48,7 +51,7 @@ public class TempAlbumServiceTest extends IntegrationTest {
         }
 
         @Test
-        void 유효한_요청이면_임시앨범을_생성한다() {
+        void 유효한_요청이면_임시_앨범을_생성한다() {
             // given
             TempAlbumCreateRequest request = new TempAlbumCreateRequest("testTitle");
 
@@ -87,6 +90,41 @@ public class TempAlbumServiceTest extends IntegrationTest {
             for (int i = 1; i <= n; i++) {
                 tempAlbumService.createTempAlbum(new TempAlbumCreateRequest("testTitle" + i));
             }
+        }
+    }
+
+    @Nested
+    class 임시_앨범_목록을_조회_할_때 {
+
+        @BeforeEach
+        void setUp() {
+            Member member =
+                    Member.createMember(
+                            OauthInfo.createOauthInfo("testOauthId", "testOauthProvider"),
+                            "testNickname",
+                            "testProfileImageUrl");
+            memberRepository.save(member);
+            given(memberUtil.getCurrentMember()).willReturn(member);
+
+            TempAlbum tempAlbum1 = TempAlbum.createTempAlbum(member, "testTitle1");
+            TempAlbum tempAlbum2 = TempAlbum.createTempAlbum(member, "testTitle2");
+            TempAlbum tempAlbum3 = TempAlbum.createTempAlbum(member, "testTitle3");
+            tempAlbumRepository.saveAll(List.of(tempAlbum1, tempAlbum2, tempAlbum3));
+        }
+
+        @Test
+        void 유효한_요청이면_임시_앨범_목록을_생성일_내림차순으로_조회한다() {
+            // when
+            TempAlbumListResponse response = tempAlbumService.getTempAlbums();
+
+            // then
+            Assertions.assertAll(
+                    () -> assertThat(response.payloads().get(0).tempAlbumId()).isEqualTo(3L),
+                    () -> assertThat(response.payloads().get(0).title()).isEqualTo("testTitle3"),
+                    () -> assertThat(response.payloads().get(1).tempAlbumId()).isEqualTo(2L),
+                    () -> assertThat(response.payloads().get(1).title()).isEqualTo("testTitle2"),
+                    () -> assertThat(response.payloads().get(2).tempAlbumId()).isEqualTo(1L),
+                    () -> assertThat(response.payloads().get(2).title()).isEqualTo("testTitle1"));
         }
     }
 }
