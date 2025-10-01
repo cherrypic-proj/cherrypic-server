a80efe46ab05fb54a3d74a2dccd96656db64bef5
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-10-01 14:56:16 +0900

    fix: 미사용 로직 제거

--- cherrypic-api/src/main/java/org/cherrypic/domain/member/repository/MemberRepository.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/member/repository/MemberRepository.java
@@ -4,14 +4,7 @@ import java.util.Optional;
 import org.cherrypic.member.entity.Member;
 import org.cherrypic.member.entity.OauthInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByOauthInfo(OauthInfo oauthInfo);
-
-    @Modifying
-    @Query("delete from Member m where m.nickname = :nickname")
-    void deleteByNickname(@Param("nickname") String nickname);
 }
