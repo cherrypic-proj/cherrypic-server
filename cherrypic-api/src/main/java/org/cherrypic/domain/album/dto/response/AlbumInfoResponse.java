b128a737a0727ee5385f0ac4b62ab85b3c10c7b3
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-10-09 18:41:11 +0900

    refactor: json 포메팅 제거

--- cherrypic-api/src/main/java/org/cherrypic/domain/album/dto/response/AlbumInfoResponse.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/album/dto/response/AlbumInfoResponse.java
@@ -1,6 +1,5 @@
 package org.cherrypic.domain.album.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 import org.cherrypic.album.enums.AlbumType;
@@ -10,9 +9,7 @@ public record AlbumInfoResponse(
         @Schema(description = "앨범 제목", example = "맛집 탐방") String title,
         @Schema(description = "앨범 커버 사진 url", example = "https://example.jpg") String coverUrl,
         @Schema(description = "앨범 유형", example = "PRO") AlbumType type,
-        @Schema(description = "사용한 용량 (GB)", example = "2.4")
-                @JsonFormat(shape = JsonFormat.Shape.STRING)
-                BigDecimal capacityUsedGb,
+        @Schema(description = "사용한 용량 (GB)", example = "2.4") BigDecimal capacityUsedGb,
         @Schema(description = "총 용량 (GB)", example = "3") BigDecimal totalCapacityGb,
         @Schema(description = "앨범 Host의 이름", example = "김OO") String hostName,
         @Schema(description = "앨범의 참여자 수", example = "6") Integer numOfParticipants) {
--- cherrypic-api/src/main/java/org/cherrypic/domain/tempalbum/dto/response/TempAlbumInfoResponse.java
+++ cherrypic-api/src/main/java/org/cherrypic/domain/tempalbum/dto/response/TempAlbumInfoResponse.java
@@ -1,6 +1,5 @@
 package org.cherrypic.domain.tempalbum.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -8,9 +7,7 @@ import org.cherrypic.global.util.StorageUnitConverter;
 
 public record TempAlbumInfoResponse(
         @Schema(description = "앨범 제목", example = "맛집 탐방") String title,
-        @Schema(description = "사용한 용량 (GB)", example = "2.4")
-                @JsonFormat(shape = JsonFormat.Shape.STRING)
-                BigDecimal capacityUsedGb,
+        @Schema(description = "사용한 용량 (GB)", example = "2.4") BigDecimal capacityUsedGb,
         @Schema(description = "총 용량 (GB)", example = "3") BigDecimal totalCapacityGb,
         @Schema(description = "만료 일자", example = "2025-01-01") LocalDate expiredAt,
         @Schema(description = "QR용 링크", example = "https://example.com") String webUrl) {
